1b9fe9f18559201eba4375ba345f263029745808
Sebastian M. Ernst <ernst@pleiszenburg.de>
2018-04-19 14:10:14 +0200

    plugin structure works

--- src/yabs/project.py
+++ src/yabs/project.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
+import importlib.util
+import os
 from pprint import pprint as pp
 
 
@@ -8,7 +10,28 @@ class project_class:
 
 
 	def __init__(self, **config):
+
 		self.config = config
+		self.config['context'] = self
+
 
 	def build(self):
-		pp(self.config)
+
+		for step in self.config['recipe']:
+
+			try:
+
+				plugin_spec = importlib.util.spec_from_file_location(
+					'plugins.%s' % step, os.path.join(os.path.dirname(__file__), 'plugins/%s.py' % step)
+					)
+				plugin = importlib.util.module_from_spec(plugin_spec)
+				plugin_spec.loader.exec_module(plugin)
+
+			except FileNotFoundError:
+
+				print('Plugin "%s" not found!' % step)
+				continue
+
+			current_plugin = plugin.plugin(**self.config)
+			current_plugin.run()
+			del current_plugin
