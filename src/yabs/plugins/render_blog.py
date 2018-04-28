e206e7ad96fdac14b658b1a978cf99e7a98ae17d
Sebastian M. Ernst <ernst@pleiszenburg.de>
2018-04-28 14:20:46 +0200

    blog text had wrapper div

--- src/yabs/plugins/render_blog.py
+++ src/yabs/plugins/render_blog.py
@@ -134,7 +134,15 @@ class blog_entry_class:
 			for h_tag in soup.find_all('h%d' % h_level):
 				h_tag.name = 'h%d' % (h_level + 2)
 
-		return str(soup) # soup.prettify()
+		out_html = str(soup) # soup.prettify()
+
+		out_html = '<div class="text">%s' % out_html
+		if '<footer' in out_html:
+			out_html = out_html.replace('<footer', '</div><footer')
+		else:
+			out_html = '%s</div>' % out_html
+
+		return out_html
 
 
 	def render(self, renderer_dict, slug_func):
