be05ff88c6f6d9bb88b9ac0dbd61ef75e16a5f8d
Sebastian M. Ernst <ernst@pleiszenburg.de>
2018-04-29 14:27:27 +0200

    code theme

--- src/yabs/plugins/fetch_styles.py
+++ src/yabs/plugins/fetch_styles.py
@@ -3,15 +3,40 @@
 
 import glob
 import os
+import subprocess
 
 
 from yabs.const import (
 	KEY_OUT,
+	KEY_PYGMENTS,
 	KEY_ROOT,
 	KEY_SRC,
-	KEY_STYLES
+	KEY_STYLES,
+	KEY_THEME
 	)
 from yabs.log import log
+
+
+def __pygmentize__(context):
+
+	cmd_list = [
+		'pygmentize',
+		'-S', context[KEY_PYGMENTS][KEY_THEME],
+		'-f', 'html', '-a', '.highlight'
+		]
+
+	proc = subprocess.Popen(
+		cmd_list,
+		stdout = subprocess.PIPE,
+		stderr = subprocess.PIPE
+		)
+	outs, errs = proc.communicate()
+
+	if errs.decode('utf-8').strip() != '':
+		log.error(errs.decode('utf-8').strip())
+
+	with open(os.path.join(context[KEY_OUT][KEY_STYLES], '%s.css' % KEY_PYGMENTS), 'w+') as f:
+		f.write(outs.decode('utf-8'))
 
 
 def run(context, options = None):
@@ -36,3 +61,5 @@ def run(context, options = None):
 
 		with open(os.path.join(context[KEY_OUT][KEY_STYLES], fn), 'w') as f:
 			f.write(cnt)
+
+	__pygmentize__(context)
