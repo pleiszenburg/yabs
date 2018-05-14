82464796554a4757cc16eb5aacce666d07fe5517
Sebastian M. Ernst <ernst@pleiszenburg.de>
2018-05-14 10:11:09 +0200

    autoescape causing too many issue - turned off

--- src/yabs/plugins/fetch_templates.py
+++ src/yabs/plugins/fetch_templates.py
@@ -19,7 +19,7 @@ def run(context, options = None):
 
 	context[KEY_JINJA] = jinja2.Environment(
 		loader = jinja2.FileSystemLoader(context[KEY_SRC][KEY_TEMPLATES], encoding = 'utf-8', followlinks = False),
-		autoescape = jinja2.select_autoescape(enabled_extensions = ('html', 'xml', 'svg', 'jinja'))
+		autoescape = False # jinja2.select_autoescape(enabled_extensions = ('html', 'xml', 'svg', 'jinja'))
 		)
 	context[KEY_TEMPLATES] = {
 		name.rsplit('.', 1)[0]: context[KEY_JINJA].get_template(name) for name in [
