9085e2182e7d76641d0311dde71b491b97bb7e70
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-24 14:14:01 +0100

    timer module

---
+++ src/yabs/timer.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+YABS
+Yet Another Build System
+https://github.com/pleiszenburg/yabs
+
+    src/yabs/timer.py: Minimal timer
+
+    Copyright (C) 2018-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/yabs/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from time import time
+
+from .abc import TimerABC
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CLASS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class Timer(TimerABC):
+    """
+    Minimal timer.
+
+    Mutable.
+    """
+
+    def __init__(self):
+
+        self._start = time()
+        self._last = self._start
+
+    def __call__(self):
+        """
+        Returns current time in second as well as elapsed time since last call in seconds.
+        """
+
+        current = time() - self._start
+        diff = current - self._last
+        self._last = current
+
+        return current, diff
