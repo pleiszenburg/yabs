848f544d680096bd72879b469412177417c809fe
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-24 18:05:31 +0100

    data is injected a single time; navigation is simply part of data

--- src/yabs/plugins/fetch_templates.py
+++ src/yabs/plugins/fetch_templates.py
@@ -8,7 +8,14 @@ import os
 import jinja2
 
 
-from yabs.const import KEY_JINJA, KEY_SRC, KEY_TEMPLATES
+from yabs.const import (
+    KEY_DATA,
+    KEY_DOMAIN,
+    KEY_JINJA,
+    KEY_SRC,
+    KEY_TEMPLATES,
+    KEY_VOCABULARY,
+)
 
 
 def run(context, options=None):
@@ -19,6 +26,15 @@ def run(context, options=None):
         ),
         autoescape=False,  # jinja2.select_autoescape(enabled_extensions = ('html', 'xml', 'svg', 'jinja'))
     )
+    context[KEY_JINJA].globals.update({
+        KEY_DOMAIN: context[KEY_DOMAIN],
+        KEY_VOCABULARY: context[KEY_VOCABULARY],
+    })
+    if KEY_DATA in context.keys():
+        context[KEY_JINJA].globals.update({
+            KEY_DATA: context[KEY_DATA],
+        })
+
     context[KEY_TEMPLATES] = {
         name.rsplit(".", 1)[0]: context[KEY_JINJA].get_template(name)
         for name in [
--- src/yabs/plugins/render_html.py
+++ src/yabs/plugins/render_html.py
@@ -6,38 +6,13 @@ import os
 
 
 from yabs.const import (
-    KEY_DATA,
-    KEY_DOMAIN,
     KEY_JINJA,
-    KEY_NAME,
-    KEY_NAVIGATION,
     KEY_OUT,
     KEY_ROOT,
-    KEY_URL,
-    KEY_VOCABULARY,
 )
 
 
-def __fix_navigation__(context):
-
-    for nav_block in context[KEY_NAVIGATION].keys():
-        for nav_lang in context[KEY_NAVIGATION][nav_block].keys():
-            lang_block_list = []
-            for nav_item_name in context[KEY_NAVIGATION][nav_block][nav_lang].keys():
-                lang_block_list.append(
-                    {
-                        KEY_NAME: nav_item_name,
-                        KEY_URL: context[KEY_NAVIGATION][nav_block][nav_lang][
-                            nav_item_name
-                        ],
-                    }
-                )
-            context[KEY_NAVIGATION][nav_block][nav_lang] = lang_block_list
-
-
 def run(context, options=None):
-
-    __fix_navigation__(context)
 
     for file_path in glob.glob(
         os.path.join(context[KEY_OUT][KEY_ROOT], "**", "*.htm*"), recursive=True
@@ -46,18 +21,7 @@ def run(context, options=None):
         with open(os.path.join(file_path), "r") as f:
             cnt = f.read()
 
-        cnt = (
-            context[KEY_JINJA]
-            .from_string(cnt)
-            .render(
-                **{
-                    KEY_DATA: context[KEY_DATA] if KEY_DATA in context.keys() else {},
-                    KEY_DOMAIN: context[KEY_DOMAIN],
-                    KEY_VOCABULARY: context[KEY_VOCABULARY],
-                },
-                **context[KEY_NAVIGATION]
-            )
-        )
+        cnt = context[KEY_JINJA].from_string(cnt).render()
 
         with open(os.path.join(file_path), "w+") as f:
             f.write(cnt)
