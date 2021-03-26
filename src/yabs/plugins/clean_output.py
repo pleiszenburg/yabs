e4ba819c1d79162a5f0507a484ef1cd76aeae1c7
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-26 19:43:24 +0100

    types, include staging

--- src/yabs/plugins/clean_output.py
+++ src/yabs/plugins/clean_output.py
@@ -3,21 +3,30 @@
 
 import os
 import shutil
+from typing import Dict
+
+from typeguard import typechecked
+
+from yabs.const import KEY_OUT, KEY_ROOT, KEY_SRC, KEY_STAGING
 
 
-from yabs.const import KEY_OUT, KEY_ROOT
+@typechecked
+def clean_folder(path: str):
 
+    for entry in os.listdir(path):
 
-def run(context, options=None):
-
-    for entry in os.listdir(context[KEY_OUT][KEY_ROOT]):
-
-        entry_path = os.path.join(context[KEY_OUT][KEY_ROOT], entry)
+        entry_path = os.path.join(path, entry)
 
         if os.path.isfile(entry_path) or os.path.islink(entry_path):
             os.unlink(entry_path)
         elif os.path.isdir(entry_path):
             shutil.rmtree(entry_path)
         else:
-            print(entry_path)
-            raise  # TODO
+            raise SystemError("not file, link or directory")
+
+
+@typechecked
+def run(context: Dict, options: None = None):
+
+    for path in (context[KEY_OUT][KEY_ROOT], context[KEY_SRC][KEY_STAGING]):
+        clean_folder(path)
