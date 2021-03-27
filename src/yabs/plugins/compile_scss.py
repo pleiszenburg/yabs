3b69657323cee44a040adc9021d4bd2e48a5f45e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-27 17:01:10 +0100

    header

--- src/yabs/plugins/compile_scss.py
+++ src/yabs/plugins/compile_scss.py
@@ -1,22 +1,53 @@
 # -*- coding: utf-8 -*-
 
+"""
+
+YABS
+Yet Another Build System
+https://github.com/pleiszenburg/yabs
+
+    src/yabs/plugins/compile_scss.py: Compiles SCSS and SASS down to CSS
+
+    Copyright (C) 2018-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/yabs/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import glob
 import os
 from typing import Dict
 
-
 import sass
 from typeguard import typechecked
 
+from ..const import KEY_COLORS, KEY_OUT, KEY_STYLES
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CONST
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 STYLE_SASS = "sass"
 STYLE_SCSS = "scss"
 STYLE_SUFFIX_LIST = [STYLE_SASS, STYLE_SCSS]
 
-
-from yabs.const import KEY_COLORS, KEY_OUT, KEY_STYLES
-
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINE
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 @typechecked
 def run(context: Dict, options: None = None):
