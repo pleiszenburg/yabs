20e8ba4375042a55f219e6565d755e9ca3b20f7b
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-27 17:04:40 +0100

    cleanup; header

--- src/yabs/plugins/compress_css.py
+++ src/yabs/plugins/compress_css.py
@@ -1,54 +1,88 @@
 # -*- coding: utf-8 -*-
 
+"""
+
+YABS
+Yet Another Build System
+https://github.com/pleiszenburg/yabs
+
+    src/yabs/plugins/compress_css.py: Compresses CSS
+
+    Copyright (C) 2018-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/yabs/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import glob
 import os
-
+from typing import Dict
 
 from bs4 import BeautifulSoup
 import csscompressor
+from typeguard import typechecked
 
+from ..const import KEY_OUT, KEY_ROOT, KEY_STYLES
 
-from yabs.const import KEY_OUT, KEY_ROOT, KEY_STYLES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-
-def compress_css(cnt):
+@typechecked
+def _compress_css(cnt: str) -> str:
 
     return csscompressor.compress(cnt)
 
 
-def compress_css_file(file_path):
+@typechecked
+def _compress_css_file(path: str):
 
-    with open(file_path, "r") as f:
+    with open(path, "r", encoding = "utf-8") as f:
         cnt = f.read()
 
-    cnt = compress_css(cnt)
+    cnt = _compress_css(cnt)
 
-    with open(file_path, "w") as f:
+    with open(path, "w", encoding = "utf-8") as f:
         f.write(cnt)
 
 
-def compress_css_in_html_file(file_path):
+@typechecked
+def _compress_css_in_html_file(path: str):
 
-    with open(file_path, "r") as f:
+    with open(path, "r", encoding = "utf-8") as f:
         cnt = f.read()
 
     soup = BeautifulSoup(cnt, "html.parser")
     for uncompressed_tag in soup.find_all("style"):
         uncompressed_str = uncompressed_tag.decode_contents()
-        compressed_str = compress_css(uncompressed_str)
+        compressed_str = _compress_css(uncompressed_str)
         cnt = cnt.replace(uncompressed_str, compressed_str)
 
-    with open(file_path, "w") as f:
+    with open(path, "w", encoding = "utf-8") as f:
         f.write(cnt)
 
 
-def run(context, options=None):
+@typechecked
+def run(context: Dict, options: None = None):
 
     for file_path in glob.glob(os.path.join(context[KEY_OUT][KEY_STYLES], "*.css")):
-        compress_css_file(file_path)
+        _compress_css_file(file_path)
 
     for file_path in glob.iglob(
         os.path.join(context[KEY_OUT][KEY_ROOT], "**/*.htm*"), recursive=True
     ):
-        compress_css_in_html_file(file_path)
+        _compress_css_in_html_file(file_path)
