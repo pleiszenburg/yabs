265bb0c4f5e1c178c02c4c91e57565e32731030f
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-27 14:58:52 +0100

    cleanup, header

--- src/yabs/plugins/clean_output.py
+++ src/yabs/plugins/clean_output.py
@@ -1,5 +1,32 @@
 # -*- coding: utf-8 -*-
 
+"""
+
+YABS
+Yet Another Build System
+https://github.com/pleiszenburg/yabs
+
+    src/yabs/plugins/clean_output.py: Removes last build
+
+    Copyright (C) 2018-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/yabs/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import os
 import shutil
@@ -9,9 +36,12 @@ from typeguard import typechecked
 
 from yabs.const import KEY_OUT, KEY_ROOT, KEY_SRC, KEY_STAGING
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 @typechecked
-def clean_folder(path: str):
+def _clean_folder(path: str):
 
     for entry in os.listdir(path):
 
@@ -29,4 +59,4 @@ def clean_folder(path: str):
 def run(context: Dict, options: None = None):
 
     for path in (context[KEY_OUT][KEY_ROOT], context[KEY_SRC][KEY_STAGING]):
-        clean_folder(path)
+        _clean_folder(path)
