a8a7d5b6aac2dc8d4f033a41022ca035b4cf7f36
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-27 19:25:59 +0100

    types, cleanup, header

--- src/yabs/plugins/fetch_styles.py
+++ src/yabs/plugins/fetch_styles.py
@@ -1,5 +1,32 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 
+"""
+
+YABS
+Yet Another Build System
+https://github.com/pleiszenburg/yabs
+
+    src/yabs/plugins/fetch_styles.py: Fetches CSS, SCSS and SASS
+
+    Copyright (C) 2018-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/yabs/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import glob
 from logging import getLogger
@@ -8,16 +35,18 @@ from typing import Dict
 
 from typeguard import typechecked
 
-from yabs.const import (
+from ..const import (
     KEY_OUT,
     KEY_SRC,
     KEY_STYLES,
     LOGGER,
 )
 
-
 _log = getLogger(LOGGER)
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 @typechecked
 def run(context: Dict, options: None = None):
