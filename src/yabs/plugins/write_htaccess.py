7464d5adcb3e637b18e111ff2e14f6a98a919f87
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-03-28 22:13:40 +0200

    header, cleanup, types

--- src/yabs/plugins/write_htaccess.py
+++ src/yabs/plugins/write_htaccess.py
@@ -1,21 +1,54 @@
 # -*- coding: utf-8 -*-
 
+"""
+
+YABS
+Yet Another Build System
+https://github.com/pleiszenburg/yabs
+
+    src/yabs/plugins/write_htaccess.py: Writes Apache .htaccess file
+
+    Copyright (C) 2018-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/yabs/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import os
 # import stat
+from typing import Dict
 
+from typeguard import typechecked
 
-from yabs.const import (
+from ..const import (
     KEY_OUT,
     KEY_ROOT,
 )
 
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-def run(context, options=None):
+@typechecked
+def run(context: Dict, options: None = None):
 
     out_path = os.path.join(context[KEY_OUT][KEY_ROOT], ".htaccess")
 
-    with open(out_path, "w") as f:
+    with open(out_path, "w", encoding = "utf-8") as f:
         f.write(options)
 
     # BUG 1und1 ...
